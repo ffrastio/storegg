fc990037f84db63287d21b6edafadb3401b56778
Fani Frastio <fanitl2a@gmail.com>
2022-02-14 08:23:54 +0700

    landing page Convert

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import { useEffect } from 'react';
+import AOS from 'aos';
 
-const Home: NextPage = () => {
+const Home: NextPage = () => { 
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
     <section>
         <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand" href="/#">
                     <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="60"
                             height="60">
@@ -33,20 +37,20 @@ const Home: NextPage = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
                         <li className="nav-item my-auto">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
                         </li>
                         <li className="nav-item my-auto">
-                            <a className="nav-link" href="#">Games</a>
+                            <a className="nav-link" href="/#">Games</a>
                         </li>
                         <li className="nav-item my-auto">
-                            <a className="nav-link" href="#">Rewards</a>
+                            <a className="nav-link" href="/#">Rewards</a>
                         </li>
 
                         <li className="nav-item my-auto">
-                            <a className="nav-link" href="#">Discover</a>
+                            <a className="nav-link" href="/#">Discover</a>
                         </li>
                         <li className="nav-item my-auto me-lg-20">
-                            <a className="nav-link" href="#">Global Rank</a>
+                            <a className="nav-link" href="/#">Global Rank</a>
                         </li>
                         <li className="nav-item my-auto">
                             <a className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
@@ -77,17 +81,17 @@ const Home: NextPage = () => {
                     <div className="d-flex flex-lg-row flex-column gap-4">
                         <a className="btn btn-get text-lg text-white rounded-pill" href="#feature" role="button">Get
                             Started</a>
-                        <a className="btn-learn text-lg color-palette-1 my-auto text-center" href="#" role="button">Learn
+                        <a className="btn-learn text-lg color-palette-1 my-auto text-center" href="/#" role="button">Learn
                             More</a>
                     </div>
                 </div>
                 <div className="col-lg-6 col-12 d-lg-block d-none">
                     <div className="d-flex justify-content-lg-end justify-content-center me-lg-5">
                         <div className="position-relative" data-aos="zoom-in">
-                            <img src="./assets/img/Header-1.png" className="img-fluid" alt=""/>
+                            <Image src="/img/Header-1.png" className="img-fluid" alt="Header 1" width={490} height={484}/>
                             <div className="card left-card position-absolute border-0">
                                 <div className="d-flex align-items-center mb-16 gap-3">
-                                    <img src="./assets/img/Header-2.png" width="40" height="40" className="rounded-pill"
+                                    <Image src="/img/Header-2.png" width={40} height={40} className="rounded-pill"
                                         alt=""/>
                                     <div>
                                         <p className="text-sm fw-medium color-palette-1 m-0">Shayna Anne</p>
@@ -129,7 +133,7 @@ const Home: NextPage = () => {
                             </div>
                             <div className="card right-card position-absolute border-0">
                                 <div className="position-relative d-flex flex-row justify-content-center mb-24">
-                                    <img src="./assets/img/Header-3.png" className="rounded-pill" alt=""/>
+                                    <Image src="/img/Header-3.png" className="rounded-pill" alt="" width={80} height={80}/>
                                     <p className="right-card-support text-white text-xxs text-center position-absolute m-0">
                                         New</p>
                                 </div>
@@ -167,14 +171,14 @@ const Home: NextPage = () => {
                                 </mask>
                                 <g mask="url(#mask1)">
                                     <path d="M-12 23H33C45.1503 23 55 32.8497 55 45V96H-12V23Z" fill="#695DE9" />
-                                    <path d="M37 55H-4" stroke="white" stroke-width="4" stroke-linecap="round"
-                                        stroke-linejoin="round" />
-                                    <path d="M23 64L-4 64" stroke="#B7B0F4" stroke-width="4" stroke-linecap="round"
-                                        stroke-linejoin="round" />
+                                    <path d="M37 55H-4" stroke="white" strokeWidth="4" strokeLinecap="round"
+                                        strokeLinejoin="round" />
+                                    <path d="M23 64L-4 64" stroke="#B7B0F4" strokeWidth="4" strokeLinecap="round"
+                                        strokeLinejoin="round" />
                                     <rect x="23" y="13" width="52" height="34" rx="13" fill="#5C52C7" />
                                 </g>
                                 <rect x="27" y="16" width="41" height="27" rx="13" fill="#2B2467" />
-                                <circle cx="47.5" cy="29.5" r="5.5" stroke="white" stroke-width="4" />
+                                <circle cx="47.5" cy="29.5" r="5.5" stroke="white" strokeWidth="4" />
                             </g>
                         </svg>
                         <p className="fw-semibold text-2xl mb-2 color-palette-1">1. Start</p>
@@ -193,15 +197,15 @@ const Home: NextPage = () => {
                             <g mask="url(#mask0-icon2)">
                                 <circle cx="40" cy="40" r="40" fill="#D7D7F8" />
                                 <rect x="-14" y="16" width="62" height="82" rx="16" fill="#695DE9" />
-                                <path d="M32 36H5" stroke="white" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M27 45L9 45" stroke="#B7B0F4" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+                                <path d="M32 36H5" stroke="white" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
+                                <path d="M27 45L9 45" stroke="#B7B0F4" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
                                 <rect x="56" y="37" width="49" height="75" rx="16" fill="#2B2467" />
-                                <path d="M97 50H70" stroke="white" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M92 59L74 59" stroke="#6B63AC" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+                                <path d="M97 50H70" stroke="white" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
+                                <path d="M92 59L74 59" stroke="#6B63AC" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
                             </g>
                         </svg>
                         <p className="fw-semibold text-2xl mb-2 color-palette-1">2. Fill Up</p>
@@ -220,12 +224,12 @@ const Home: NextPage = () => {
                             <g mask="url(#mask0-icon3)">
                                 <circle cx="40" cy="40" r="40" fill="#D7D7F8" />
                                 <rect x="13" y="25" width="54" height="61" rx="16" fill="#695DE9" />
-                                <path d="M50 46L50 68" stroke="white" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M40 54L40 68" stroke="#B7B0F4" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M30 60L30 68" stroke="#B7B0F4" stroke-width="4" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+                                <path d="M50 46L50 68" stroke="white" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
+                                <path d="M40 54L40 68" stroke="#B7B0F4" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
+                                <path d="M30 60L30 68" stroke="#B7B0F4" strokeWidth="4" strokeLinecap="round"
+                                    strokeLinejoin="round" />
                                 <circle cx="40" cy="26" r="14" fill="#2B2467" />
                                 <path
                                     d="M39.0489 18.927C39.3483 18.0057 40.6517 18.0057 40.9511 18.9271L42.0206 22.2188C42.1545 22.6309 42.5385 22.9098 42.9717 22.9098H46.4329C47.4016 22.9098 47.8044 24.1494 47.0207 24.7188L44.2205 26.7533C43.87 27.0079 43.7234 27.4593 43.8572 27.8713L44.9268 31.1631C45.2261 32.0844 44.1717 32.8506 43.388 32.2812L40.5878 30.2467C40.2373 29.9921 39.7627 29.9921 39.4122 30.2467L36.612 32.2812C35.8283 32.8506 34.7739 32.0844 35.0732 31.1631L36.1428 27.8713C36.2766 27.4593 36.13 27.0079 35.7795 26.7533L32.9793 24.7188C32.1956 24.1494 32.5984 22.9098 33.5671 22.9098H37.0283C37.4615 22.9098 37.8455 22.6309 37.9794 22.2188L39.0489 18.927Z"
@@ -250,7 +254,7 @@ const Home: NextPage = () => {
                 <div className="featured-game-card position-relative">
                     <a href="./src/detail.html">
                         <div className="blur-sharp">
-                            <img src="./assets/img/Thumbnail-1.png" width="205" height="270" alt=""/>
+                            <Image src="/img/Thumbnail-1.png" width={205} height={270} alt=""/>
                         </div>
                         <div className="cover position-absolute bottom-0 m-32">
                             <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
@@ -274,7 +278,7 @@ const Home: NextPage = () => {
                 <div className="featured-game-card position-relative">
                     <a href="./src/detail.html">
                         <div className="blur-sharp">
-                            <img src="./assets/img/Thumbnail-2.png" width="205" height="270" alt=""/>
+                            <Image src="/img/Thumbnail-2.png" width={205} height={270} alt=""/>
                         </div>
                         <div className="cover position-absolute bottom-0 m-32">
                             <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
@@ -298,7 +302,7 @@ const Home: NextPage = () => {
                 <div className="featured-game-card position-relative">
                     <a href="./src/detail.html">
                         <div className="blur-sharp">
-                            <img src="./assets/img/Thumbnail-3.png" width="205" height="270" alt=""/>
+                            <Image src="/img/Thumbnail-3.png" width={205} height={270} alt=""/>
                         </div>
                         <div className="cover position-absolute bottom-0 m-32">
                             <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
@@ -322,7 +326,7 @@ const Home: NextPage = () => {
                 <div className="featured-game-card position-relative">
                     <a href="./src/detail.html">
                         <div className="blur-sharp">
-                            <img src="./assets/img/Thumbnail-4.png" width="205" height="270" alt=""/>
+                            <Image src="/img/Thumbnail-4.png" width={205} height={270} alt=""/>
                         </div>
                         <div className="cover position-absolute bottom-0 m-32">
                             <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
@@ -346,7 +350,7 @@ const Home: NextPage = () => {
                 <div className="featured-game-card position-relative">
                     <a href="./src/detail.html">
                         <div className="blur-sharp">
-                            <img src="./assets/img/Thumbnail-5.png" width="205" height="270" alt=""/>
+                            <Image src="/img/Thumbnail-5.png" width={205} height={270} alt=""/>
                         </div>
                         <div className="cover position-absolute bottom-0 m-32">
                             <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
@@ -404,7 +408,7 @@ const Home: NextPage = () => {
         <div className="container-xxl container-fluid">
             <div className="row align-items-center px-lg-5 mx-auto gap-lg-0 gap-4">
                 <div className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60" data-aos="zoom-in">
-                    <img src="./assets/img/Header-9.png" width="612" height="452" className="img-fluid" alt=""/>
+                    <Image src="/img/Header-9.png" width={612} height={452} className="img-fluid" alt=""/>
                 </div>
                 <div className="col-lg-5 col-12 ps-lg-60">
                     <div className="">
@@ -414,7 +418,7 @@ const Home: NextPage = () => {
                                 className="d-sm-block d-none"/>
                             membantu players menjadi<br className="d-sm-block d-none"/> pemenang sejati</p>
                         <div className="d-md-block d-flex flex-column w-100">
-                            <a className="btn btn-read text-lg rounded-pill" href="#" role="button">Read Story</a>
+                            <a className="btn btn-read text-lg rounded-pill" href="/#" role="button">Read Story</a>
                         </div>
                     </div>
                 </div>
@@ -427,7 +431,7 @@ const Home: NextPage = () => {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-4 text-lg-start text-center">
-                        <a href="" className="mb-30">
+                        <a href="/" className="mb-30">
                             <svg width="60" height="60" viewBox="0 0 60 60" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="60"
@@ -455,17 +459,17 @@ const Home: NextPage = () => {
                                 <p className="text-lg fw-semibold color-palette-1 mb-12">Company</p>
                                 <ul className="list-unstyled">
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">About Us</a>
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">About Us</a>
                                     </li>
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Press
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Press
                                             Release</a>
                                     </li>
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Terms of Use</a>
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Terms of Use</a>
                                     </li>
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Privacy &
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Privacy &
                                             Policy</a>
                                     </li>
                                 </ul>
@@ -474,15 +478,15 @@ const Home: NextPage = () => {
                                 <p className="text-lg fw-semibold color-palette-1 mb-12">Support</p>
                                 <ul className="list-unstyled">
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Refund
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Refund
                                             Policy</a>
                                     </li>
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Unlock
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Unlock
                                             Rewards</a>
                                     </li>
                                     <li className="mb-6">
-                                        <a href="" className="text-lg color-palette-1 text-decoration-none">Live
+                                        <a href="/" className="text-lg color-palette-1 text-decoration-none">Live
                                             Chatting</a>
                                     </li>
                                 </ul>
@@ -518,7 +522,7 @@ const Home: NextPage = () => {
         </footer>
     </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
